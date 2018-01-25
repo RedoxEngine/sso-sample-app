f7b7f894d7e35149420bb6b2d890dc3a79ee6985
nwhatt <nick@redoxengine.com>
2018-01-25 11:02:27 -0500

    test automatic deploys

--- sso_test.go
+++ sso_test.go
@@ -19,6 +19,6 @@ func TestRedirectHandler(t *testing.T) {
 	body, _ := ioutil.ReadAll(resp.Body)
 
 	assert.Equal(t, 302, resp.StatusCode, "Should return a 302 response")
-	assert.Equal(t, "<a href=\"https://www.youtube.com/embed/dQw4w9WgXcQ\">Found</a>.\n\n", string(body), "Should return an anchor tag in the body")
+	assert.Equal(t, "<a href=\"\">Found</a>.\n\n", string(body), "Should return an anchor tag in the body")
 	fmt.Println(resp.StatusCode)
 }
